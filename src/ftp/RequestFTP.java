1d9122994a9599591932ca977778aa66dba8a8f1
Mahieddine Yaker <mahieddine.yaker@gmail.com>
2015-01-27 18:53:05 +0100

    continuer sur la fonction send.

--- src/ftp/RequestFTP.java
+++ src/ftp/RequestFTP.java
@@ -1,24 +1,105 @@
 package ftp;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.HashMap;
 
-public class RequestFTP {
+public class RequestFTP implements Runnable {
 
-	private String commande;
+	private HashMap<String, String> usersList;
+	private Socket socket;
+	private String user;
+	private String pass;
 	
-	public RequestFTP(Socket cmd){
-		
+	private boolean connected;
+	private String action;
+	
+	public RequestFTP(Socket s,HashMap<String, String> ul){
+		this.socket = s;
+		this.connected= false;
+		this.usersList =  ul ;
 	}
 	public void processRequest() {
+		String buffer;
+		send("220");
+		while(true){
+			try{
+				
+				InputStream is = this.socket.getInputStream();
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader br = new BufferedReader(isr);
+				buffer = br.readLine();
+				checkRequest(buffer);
+
+			}catch (IOException e){
+				
+			}
+		}
 		
 	}
 
+	private void checkRequest(String buffer){
+		String[] split = buffer.split(" ");
+		switch(split[0]){
+		case "USER":
+			if(split.length >= 2){
+				this.action = split[1];
+				processUser();
+			}else {
+				send("USER <pseudo>");
+			}
+			break;
+		case "PASS":
+			if(split.length >= 2){
+				this.action = split[1];
+				processUser();
+			}else {
+				send("USER <pseudo>");
+			}
+			break;
+		default:
+			send("505");
+			
+		}
+	}
+	
+	private void send(String mess){
+		System.out.println("Send de : "+mess);
+		OutputStream os;
+		try {
+			os = this.socket.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(os);
+			dos.write(mess.toCharArray());
+			dos.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public void processUser() {
-
+		HashMap<String, String> users = this.usersList;
+		if(users.containsKey(this.action)){
+			this.user = this.action;
+			send("331");
+		}else {
+			send("332");
+		}
 	}
 
 	public void processPass() {
-
+		HashMap<String, String> users = this.usersList;
+		if(users.containsValue(this.action)){
+			this.pass = this.action;
+			send("230");
+		}else {
+			send("332");
+		}
 	}
 
 	public void processRetr() {
@@ -35,6 +116,11 @@ public class RequestFTP {
 
 	public void processQuit() {
 
+	}
+
+	public void run() {
+
+		this.processRequest();
 	}
 
 }
--- src/ftp/Server.java
+++ src/ftp/Server.java
@@ -3,24 +3,25 @@ package ftp;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
 
 public class Server {
 
+
 	public static void main(String[] args) {
-		if(args.length !=1){
-			System.err.println("Port non specifié");
-		}
+		HashMap<String, String> usersList = new HashMap<String,String>();
+		usersList.put("mah", "toto");
 		/*creation serversocket*/
 		try {
 			@SuppressWarnings("resource")
-			final ServerSocket ss = new ServerSocket(Integer.parseInt(args[0]));
+			final ServerSocket ss = new ServerSocket(2783);
 			/*fin creation serversocket*/
 			Socket socket =null;
 			/*accepter une connection passer la main a un thread*/
 			while(true){
 				socket = ss.accept();
-				Thread tFtp =new Thread(new RequestFTP(socket));
+				Thread tFtp =new Thread(new RequestFTP(socket,usersList));
 				tFtp.start();
 			}
 		} catch (NumberFormatException e) {
